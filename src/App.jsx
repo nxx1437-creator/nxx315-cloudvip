9c59d12d729487c2e8fffd37e459617bf544b99c
nxx1437-creator <nxx1437@gmail.com>
2026-08-18 08:11:03 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import CloudVIPLanding from "./CloudVIPLanding.jsx";
+import Login from "./Login.jsx";
+import Register from "./Register.jsx";
 
 export default function App() {
-  return <CloudVIPLanding />;
+  return (
+    <Routes>
+      <Route path="/" element={<CloudVIPLanding />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 }
