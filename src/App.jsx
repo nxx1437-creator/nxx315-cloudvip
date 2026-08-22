2191b17ea3fbbc6da08988c9d9aec847f4e7dcdd
nxx1437-creator <nxx1437@gmail.com>
2026-08-22 09:06:08 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,36 @@ import Register from "./pages/Register.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Tasks from "./pages/Tasks.jsx";
 import Store from "./pages/Store.jsx";
+import Admin from "./pages/Admin.jsx";
+import TaskCallback from "./pages/TaskCallback.jsx";
 import Wallet from "./pages/Wallet.jsx";
 import ProfilePage from "./pages/Profile.jsx";
 import Marketing from "./pages/Marketing.jsx";
 import MarketingWallet from "./pages/MarketingWallet.jsx";
-
-import Admin from "./pages/Admin.jsx";
-import TaskCallback from "./pages/TaskCallback.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
 
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 import AdminRoute from "./components/AdminRoute.jsx";
+
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<CloudVIPLanding />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/wallet" element={<ProtectedRoute><Wallet /></ProtectedRoute>} />
-      <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
-      <Route path="/marketing" element={<ProtectedRoute><Marketing /></ProtectedRoute>} /> {/* 👈 Thêm dòng này */}
-      
+
+      <Route
+        path="/"
+        element={<CloudVIPLanding />}
+      />
+
+      <Route
+        path="/login"
+        element={<Login />}
+      />
+
+      <Route
+        path="/register"
+        element={<Register />}
+      />
+
       <Route
         path="/dashboard"
         element={
@@ -37,26 +46,84 @@ export default function App() {
           </ProtectedRoute>
         }
       />
-      {/* ... Các route khác giữ nguyên ... */}
-      
-      <Route path="*" element={<NotFoundPage />} />
+
+      <Route
+        path="/tasks"
+        element={
+          <ProtectedRoute>
+            <Tasks />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/store"
+        element={
+          <ProtectedRoute>
+            <Store />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/wallet"
+        element={
+          <ProtectedRoute>
+            <Wallet />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <ProfilePage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/marketing"
+        element={
+          <ProtectedRoute>
+            <Marketing />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/marketing-wallet"
+        element={
+          <ProtectedRoute>
+            <MarketingWallet />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/task-callback"
+        element={
+          <ProtectedRoute>
+            <TaskCallback />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/admin"
+        element={
+          <AdminRoute>
+            <Admin />
+          </AdminRoute>
+        }
+      />
+
+      <Route
+        path="*"
+        element={<NotFoundPage />}
+      />
+
     </Routes>
   );
 }
-<Route
-  path="/marketing"
-  element={
-    <ProtectedRoute>
-      <Marketing />
-    </ProtectedRoute>
-  }
-/>
-
-<Route
-  path="/marketing-wallet"
-  element={
-    <ProtectedRoute>
-      <MarketingWallet />
-    </ProtectedRoute>
-  }
-/>
