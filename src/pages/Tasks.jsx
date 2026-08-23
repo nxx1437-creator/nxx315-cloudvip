57c5111e2d64347b142867c1a2d5d8d71997f81f
nxx1437-creator <nxx1437@gmail.com>
2026-08-23 10:04:32 +0700

    Update Tasks.jsx

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -307,7 +307,7 @@ export default function Tasks() {
 import ReCAPTCHA from "react-google-recaptcha";
 
 // 1. Khai báo Site Key lấy từ Google
-const RECAPTCHA_SITE_KEY = "YOUR_SITE_KEY_HERE";
+const RECAPTCHA_SITE_KEY = "6LeWCZQtAAAAAHFMcZ7gCgrmRvkuiIrqWq7Odmrm";
 
 // 2. Thêm state để lưu trạng thái xác thực
 const [captchaToken, setCaptchaToken] = useState(null);
