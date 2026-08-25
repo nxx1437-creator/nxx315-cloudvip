05bacc1968a3a74380f58248e42dad2b6abd9bbd
nxx1437-creator <nxx1437@gmail.com>
2026-08-25 20:12:20 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import MaintenanceBanner from './components/MaintenanceBanner.jsx'; // 👈 Import
 
-// Pages
 import Home from './CloudVIPLanding.jsx';
 import Tasks from './pages/Tasks.jsx';
 import Store from './pages/Store.jsx';
@@ -10,16 +8,10 @@ import Admin from './pages/Admin.jsx';
 import Login from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
 import Status from './pages/Status.jsx';
-import ProtectedRoute from './components/ProtectedRoute.jsx';
 
 export default function App() {
-  const MAINTENANCE_MODE = true; // 👈 Bật/tắt bảo trì ở đây
-
   return (
     <HashRouter>
-      {/* 👉 Banner bảo trì hiển thị trên MỌI trang */}
-      {MAINTENANCE_MODE && <MaintenanceBanner />}
-      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tasks" element={<Tasks />} />
