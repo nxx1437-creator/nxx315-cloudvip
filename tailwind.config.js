41c0f44c39f0a4948da1612d38be6dd0c05b3fe9
nxx1437-creator <nxx1437@gmail.com>
2026-09-01 08:38:02 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: {
+          400: "rgb(var(--accent-400) / <alpha-value>)",
+          500: "rgb(var(--accent-500) / <alpha-value>)",
+          600: "rgb(var(--accent-600) / <alpha-value>)",
+        },
+      },
+    },
   },
   plugins: [],
 };
