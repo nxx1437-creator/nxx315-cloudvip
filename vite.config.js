7f0cab8156faff909bb3bccc104f9eb97e8be28e
nxx1437-creator <nxx1437@gmail.com>
2026-09-05 14:52:27 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+
+function versionPlugin() {
+  return {
+    name: 'version-plugin',
+    buildStart() {
+      const version = Date.now().toString();
+      fs.writeFileSync('public/version.json', JSON.stringify({ version }));
+    },
+  };
+}
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), versionPlugin()],
 });
