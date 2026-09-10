ed1fabf46303df47225fa80672642f417604cce2
nxx1437-creator <nxx1437@gmail.com>
2026-09-10 18:27:51 +0700

    Update roblox-user.js

--- api/roblox-user.js
+++ api/roblox-user.js
@@ -1,7 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = process.env.ROBLOX_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.ROBLOX_SUPABASE_ANON_KEY;
+const SUPABASE_URL =
+  process.env.ROBLOX_SUPABASE_URL;
+
+const SUPABASE_ANON_KEY =
+  process.env.ROBLOX_SUPABASE_ANON_KEY;
 
 export default async function handler(req, res) {
   if (req.method !== "GET") {
