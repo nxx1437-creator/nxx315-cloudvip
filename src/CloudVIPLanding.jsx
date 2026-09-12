54cef1a2973f33d040b90b3e3a57c7bb44965198
nxx1437-creator <nxx1437@gmail.com>
2026-09-12 17:26:24 +0700

    Update CloudVIPLanding.jsx

--- src/CloudVIPLanding.jsx
+++ src/CloudVIPLanding.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import { supabase } from "./lib/supabaseClient.jsx";
+import { supabase } from "../lib/supabaseClient.js";
 import {
   Coins,
   ShieldCheck,
