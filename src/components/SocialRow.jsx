627da025bd073a8c803d2cb4750feeba76c47e8c
nxx1437-creator <nxx1437@gmail.com>
2026-09-22 13:01:57 +0700

    Update SocialRow.jsx

--- src/components/SocialRow.jsx
+++ src/components/SocialRow.jsx
@@ -19,16 +19,6 @@ export default function SocialRow({ onSelect }) {
       ),
     },
     {
-      key: "facebook",
-      label: "Continue with Facebook",
-      supported: true,
-      icon: (
-        <svg width="20" height="20" viewBox="0 0 24 24" fill="#1877F2">
-          <path d="M22 12.06C22 6.5 17.52 2 12 2S2 6.5 2 12.06c0 5 3.66 9.15 8.44 9.94v-7.03H7.9v-2.91h2.54V9.85c0-2.5 1.49-3.89 3.77-3.89 1.09 0 2.23.2 2.23.2v2.46h-1.26c-1.24 0-1.63.77-1.63 1.56v1.88h2.78l-.44 2.91h-2.34V22c4.78-.79 8.44-4.94 8.44-9.94Z" />
-        </svg>
-      ),
-    },
-    {
       key: "discord",
       label: "Continue with Discord",
       supported: true,
@@ -45,7 +35,7 @@ export default function SocialRow({ onSelect }) {
     setLoadingKey(item.key);
 
     // Đợi 400ms để React render trạng thái loading
-    await new Promise((r) => setTimeout(r, 700));
+    await new Promise((r) => setTimeout(r, 800));
 
     try {
       await onSelect(item.key, item.supported);
